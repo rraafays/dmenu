bda1c8313d648571e919729c96f14dd81521963a
raf <rraf@tuta.io>
2023-01-09 17:11:19 +0000

    update: changed to gruvbox colours

--- config.def.h
+++ config.def.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#EBDBB2", "#282828" },
+	[SchemeSel] = { "#282828", "#EBDBB2" },
+	[SchemeOut] = { "#282828", "#EBDBB2" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
