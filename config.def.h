e01f79cff309543576c666ddc29f2982a3602a78
raf <rraf@tuta.io>
2023-01-10 08:41:11 +0000

    update: migrated config.def.h customisations to config.h, will edit this file going forward instead of config.def.h

--- config.def.h
+++ config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka Term:size=10"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#EBDBB2", "#282828" },
-	[SchemeSel] = { "#282828", "#EBDBB2" },
-	[SchemeOut] = { "#282828", "#EBDBB2" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
